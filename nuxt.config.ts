99e214ec0989816d708532ce4e97d8fc0bd46a80
Kasefuchs <kasefuchs@protonmail.com>
2024-11-30 19:59:53 +0300

    Use prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,35 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
 
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=1920, height=1080, initial-scale=1',
-      title: 'Foxogram - New discovery in the world of messengers',
-      meta: [
-        { property: 'og:image', content: '/image/favicon.svg' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.svg' }
-      ],
+      charset: "utf-8",
+      viewport: "width=1920, height=1080, initial-scale=1",
+      title: "Foxogram - New discovery in the world of messengers",
+      meta: [{ property: "og:image", content: "/image/favicon.svg" }],
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.svg" }],
     },
   },
 
-  modules: ['@pinia/nuxt', '@nuxt/fonts'],
+  modules: ["@pinia/nuxt", "@nuxt/fonts"],
 
-  css: ['@/assets/css/default.css'],
+  css: ["@/assets/css/default.css"],
 
   runtimeConfig: {
     public: {
-      apiBase: 'https://api-dev.foxogram.su/',
+      apiBase: "https://api-dev.foxogram.su/",
     },
   },
 
   devtools: {
-    enabled: true
+    enabled: true,
+  },
+
+  ssr: false,
+
+  hooks: {
+    "prerender:routes"({ routes }) {
+      routes.clear();
+    },
   },
 });
