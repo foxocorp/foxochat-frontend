1733e970e45b745eba68c95331207992746fe42c
SnakOleg <ochsherbina@gmail.com>
2024-12-16 03:05:13 +0500

    Add more weight for font

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import "preact/debug"; // disable in production
 
-import "@fontsource/inter";
 import "@fontsource/inter/900.css";
 import "@fontsource/inter/800.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/500.css";
+import "@fontsource/inter"; //400
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/200.css";
 import "./style.css";
 
 import { render } from "preact";
@@ -16,18 +18,18 @@ import Login from "./pages/Auth/Login.tsx";
 import Register from "./pages/Auth/Register.tsx";
 
 export function App() {
-	return (
-		<LocationProvider>
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route path="/auth/login" component={Login} />
-					<Route path="/auth/register" component={Register} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-		</LocationProvider>
-	);
+    return (
+        <LocationProvider>
+            <main>
+                <Router>
+                    <Route path="/" component={Home}/>
+                    <Route path="/auth/login" component={Login}/>
+                    <Route path="/auth/register" component={Register}/>
+                    <Route default component={NotFound}/>
+                </Router>
+            </main>
+        </LocationProvider>
+    );
 }
 
-render(<App />, document.getElementById("app")!);
+render(<App/>, document.getElementById("app")!);
