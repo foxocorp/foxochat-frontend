97540eb9e580490a0473fde860f4192ee098ce84
SnakOleg <ochsherbina@gmail.com>
2024-12-18 22:01:57 +0500

    Add loading and fix background color

--- src/components/LoadingApp.tsx
+++ src/components/LoadingApp.tsx
@@ -1,30 +1,36 @@
 import { useState, useEffect } from "preact/hooks";
-import "./LoadingApp.module.css";
+import styles from "./LoadingApp.module.css";
+import foxogramLogo from "../assets/foxogram.svg";
 
-const Loading = () => {
+const Loading = ({ onLoaded }: { onLoaded: () => void }) => {
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		const handleLoad = () => {
 			setIsLoading(false);
+			onLoaded();
 		};
 
-		window.addEventListener("load", handleLoad);
+		if (document.readyState === "complete") {
+			handleLoad();
+		} else {
+			window.addEventListener("load", handleLoad);
+		}
 
 		return () => {
 			window.removeEventListener("load", handleLoad);
 		};
-	}, []);
+	}, [onLoaded]);
 
 	if (!isLoading) {
 		return null;
 	}
 
 	return (
-		<div className="loading-overlay">
-			<img src="../assets/foxogram-logo.svg" alt="logo" className="logo" />
-			<div className="loading">
-				<div className="loading-bar" />
+		<div className={styles["loading-overlay"]}>
+			<img src={foxogramLogo} alt="Logo" className={styles["logo"]} />
+			<div className={styles["loading"]}>
+				<div className={styles["loading-bar"]} />
 			</div>
 		</div>
 	);
