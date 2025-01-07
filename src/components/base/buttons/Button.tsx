d91f7d1c07f3f5911796dbd904b23a87e7bac336
SnakOleg <ochsherbina@gmail.com>
2025-01-07 18:52:10 +0500

    Add types for button

--- src/components/base/buttons/Button.tsx
+++ src/components/base/buttons/Button.tsx
@@ -8,13 +8,14 @@ interface ButtonProps {
 	variant?: "primary" | "secondary" | "danger" | "default";
 	icon?: string | undefined;
 	disabled?: boolean;
+	type?: "button" | "submit" | "reset" | undefined;
 }
 
-export function Button({ children, width = 368, variant = "default", onClick, icon, disabled = false }: ButtonProps) {
+export function Button({ children, width = 368, variant = "default", onClick, icon, disabled = false, type }: ButtonProps) {
 	const buttonClass = `button-${variant} ${disabled ? "button-disabled" : ""}`;
 
 	return (
-		<button className={buttonClass} style={{ width }} onClick={onClick} disabled={disabled} type="button">
+		<button className={buttonClass} style={{ width }} onClick={onClick} disabled={disabled} type={type}>
 			<span className="button-text">{children}</span>
 			{icon && <img src={icon} alt="icon" className="button-icon" />}
 		</button>
