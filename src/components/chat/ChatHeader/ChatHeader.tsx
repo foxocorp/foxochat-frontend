14255edb82ef1c980388353aadb40bcd1d312dda
SnakOleg <ochsherbina@gmail.com>
2025-01-09 22:32:20 +0500

    Add chat header for chat window

---
+++ src/components/chat/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,35 @@
+import styles from "./ChatHeader.module.css";
+
+interface ChatHeaderProps {
+    avatar: string | null;
+    displayName?: string | null;
+    username: string;
+    status: string;
+}
+
+const ChatHeader = ({ avatar, displayName, username, status }: ChatHeaderProps) => {
+    const nameToDisplay = displayName || username;
+
+    return (
+        <div className={styles["chat-header"]}>
+            {avatar ? (
+                <img
+                    src={avatar}
+                    alt={`${nameToDisplay}'s avatar`}
+                    className={styles["chat-header-avatar"]}
+                />
+            ) : (
+                <div className={styles["default-avatar"]}>
+                    {nameToDisplay.charAt(0).toUpperCase()}
+                </div>
+            )}
+            <div className={styles["chat-header-info"]}>
+                <p className={styles["chat-header-username"]}>{nameToDisplay}</p>
+                <p className={styles["chat-header-status"]}>{status}</p>
+            </div>
+            <button className={styles["chat-header-edit"]}></button>
+        </div>
+    );
+};
+
+export default ChatHeader;
