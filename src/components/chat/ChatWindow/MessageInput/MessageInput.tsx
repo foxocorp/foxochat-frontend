5b6bc85f022b5a9f091cc5bf62533069cb2a8179
SnakOleg <ochsherbina@gmail.com>
2025-01-09 22:32:59 +0500

    Add message input for chat window

---
+++ src/components/chat/ChatWindow/MessageInput/MessageInput.tsx
@@ -0,0 +1,52 @@
+import { useState } from "preact/hooks";
+import styles from "./MessageInput.module.css";
+import mediaIcon from "@icons/chat/media.svg";
+import sendIcon from "@icons/chat/paperplane.svg";
+import React from "react";
+
+interface MessageInputProps {
+    onSendMessage: (message: string) => void;
+    onSendMedia?: () => void;
+}
+
+const MessageInput = ({ onSendMessage, onSendMedia }: MessageInputProps) => {
+    const [message, setMessage] = useState("");
+
+    const handleSend = () => {
+        if (message.trim() !== "") {
+            onSendMessage(message);
+            setMessage("");
+        }
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && message.trim() !== "") {
+            handleSend();
+        }
+    };
+
+    return (
+        <div className={styles["message-input-container"]}>
+            <div className={styles["message-input-background"]}>
+                {onSendMedia && (
+                    <button onClick={onSendMedia} className={styles["icon-button"]}>
+                        <img src={mediaIcon} alt="Media" className={styles["icon"]} />
+                    </button>
+                )}
+                <input
+                    type="text"
+                    value={message}
+                    onInput={(e: any) => setMessage(e.target.value)}
+                    placeholder="Write your message..."
+                    className={styles["message-input"]}
+                    onKeyDown={handleKeyDown}
+                />
+                <button onClick={handleSend} className={styles["icon-button"]}>
+                    <img src={sendIcon} alt="Send" className={styles["icon"]} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default MessageInput;
