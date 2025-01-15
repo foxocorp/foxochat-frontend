652ed44cf6a91bc5008ad839ff57d27978ec734b
kasefuchs <kasefuchs@protonmail.com>
2025-01-15 23:16:32 +0300

    Refactor logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,43 +1,50 @@
-type LogLevel = 'debug' | 'info' | 'warn' | 'error';
+export enum LogLevel {
+    Debug = 1,
+    Info = 2,
+    Warn = 4,
+    Error = 8,
+}
 
 export class Logger {
-    private static colors = {
-        info: '\x1b[32m',
-        warn: '\x1b[33m',
-        error: '\x1b[31m',
-        debug: '\x1b[37m',
-    }
+    private static colors: Record<LogLevel, string> = {
+        [LogLevel.Info]: "\x1b[32m",
+        [LogLevel.Warn]: "\x1b[33m",
+        [LogLevel.Error]: "\x1b[31m",
+        [LogLevel.Debug]: "\x1b[37m",
+    };
 
-    private static logLevels: Record<string, LogLevel[]> = {
-        production: ['info', 'error'],
-        development: ['debug', 'info', 'warn', 'error'],
-    }
+    private static logLevels: Record<string, LogLevel> = {
+        production: LogLevel.Info | LogLevel.Error,
+        development: LogLevel.Debug | LogLevel.Info | LogLevel.Warn | LogLevel.Error,
+    };
 
-    private static currentEnv: string = import.meta.env.MODE || 'development';
+    private static currentEnv: string = import.meta.env.MODE;
 
     private static isLogLevelEnabled(level: LogLevel): boolean {
-        return this.logLevels[this.currentEnv]?.includes(level) ?? false;
+        const envMask = this.logLevels[this.currentEnv];
+
+        return !!envMask && (level & envMask) === level.valueOf();
     }
 
     private static logMessage(level: LogLevel, message: string) {
         if (this.isLogLevelEnabled(level)) {
-            console.log(`${this.colors[level]}[${level.toUpperCase()}] ${message}`);
+            console.log(`${this.colors[level]}[${LogLevel[level]}] ${message}`);
         }
     }
 
     static debug(message: string) {
-        this.logMessage('debug', message);
+        this.logMessage(LogLevel.Debug, message);
     }
 
     static info(message: string) {
-        this.logMessage('info', message);
+        this.logMessage(LogLevel.Info, message);
     }
 
     static warn(message: string) {
-        this.logMessage('warn', message);
+        this.logMessage(LogLevel.Warn, message);
     }
 
     static error(message: string) {
-        this.logMessage('error', message);
+        this.logMessage(LogLevel.Error, message);
     }
 }
