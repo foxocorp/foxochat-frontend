b0996075d30dc4147817791bd4794f02b5483b5b
SnakOleg <ochsherbina@gmail.com>
2025-01-22 18:26:40 +0500

    Add new rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,7 @@ export default tseslint.config(
 				allowFunctions: true,
 				allowArrowFunctions: true,
 			}],
+			"eslint-disable-next-line react/no-danger": 2,
 			"react/jsx-no-comment-textnodes": 2,
 			"react/jsx-no-duplicate-props": 2,
 			"react/jsx-no-target-blank": 2,
