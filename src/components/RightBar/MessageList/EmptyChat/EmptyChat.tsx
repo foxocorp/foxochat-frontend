b9e374627c6bb1483559cf9d75087067c70428ff
snakoleg <ochsherbina@gmail.com>
2025-06-02 19:33:24 +0300

    Improve empty chat styling

--- src/components/RightBar/MessageList/EmptyChat/EmptyChat.tsx
+++ src/components/RightBar/MessageList/EmptyChat/EmptyChat.tsx
@@ -4,15 +4,13 @@ import styles from "./EmptyChat.module.scss";
 const EmptyChat = () => {
 	return (
 		<div className={styles.emptyChat}>
-			<div className={styles.icon}>
-				<span role="img" aria-label="chat-icon">
-					{renderEmojisToJSX("🤔")}
-				</span>
+			<div className={styles.emptyChatContainer}>
+				<h2 className={styles.title}>It’s kind of empty here...</h2>
+				<p className={styles.description}>
+					Would you like to fix it? Start the conversation first!
+				</p>
 			</div>
-			<h2 className={styles.title}>It’s kind of empty here...</h2>
-			<p className={styles.description}>
-				Would you like to fix it? Start the conversation first!
-			</p>
+			<span className={styles.icon}>{renderEmojisToJSX("🤔", "160")}</span>
 		</div>
 	);
 };
