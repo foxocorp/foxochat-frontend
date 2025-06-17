669920a327a5bf54ec7e6fc9b981ce188d589021
snakoleg <ochsherbina@gmail.com>
2025-06-17 07:39:02 +0300

    Add context menu to ChatItem

--- src/components/LeftBar/ChatList/ChatItem/ChatItem.tsx
+++ src/components/LeftBar/ChatList/ChatItem/ChatItem.tsx
@@ -1,10 +1,20 @@
 import ChannelIcon from "@/assets/icons/left-bar/chat-list/channel.svg";
 import GroupIcon from "@/assets/icons/left-bar/chat-list/group.svg";
+import EditIcon from "@/assets/icons/right-bar/chat/chat-overview/edit.svg";
+import MarkAsReadIcon from "@/assets/icons/right-bar/chat/chat-overview/mark-as-read.svg";
+import MuteIcon from "@/assets/icons/right-bar/chat/chat-overview/mute.svg";
+import PinIcon from "@/assets/icons/right-bar/chat/chat-overview/pin.svg";
+import PreviewIcon from "@/assets/icons/right-bar/chat/chat-overview/preview.svg";
+import TrashIcon from "@/assets/icons/right-bar/chat/chat-overview/trash.svg";
+import ContextMenu from "@components/Base/ContextMenu/ContextMenu";
+import { useContextMenu } from "@components/Base/ContextMenu/useContextMenu";
 import { ExtendedChatItemProps } from "@interfaces/interfaces";
+import { apiMethods } from "@services/API/apiMethods";
 import { renderEmojisToJSX } from "@utils/emoji";
 import { ChannelType } from "foxochat.js";
 import { toJS } from "mobx";
 import { observer } from "mobx-react";
+import React from "preact/compat";
 import { ChatAvatar } from "./ChatAvatar";
 import * as styles from "./ChatItem.module.scss";
 
@@ -60,28 +70,104 @@ const ChatItemComponent = ({
 		}
 	};
 
+	const contextMenu = useContextMenu();
+
+	const isOwner = chat.owner?.id === currentUser;
+
+	const handleContextMenu = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.preventDefault();
+		const items = [
+			{
+				icon: <img src={PinIcon} alt="Pin" />,
+				label: "Pin",
+				onClick: () => {
+					/* TODO: pin logic */
+				},
+			},
+			...(isOwner
+				? [
+						{
+							icon: <img src={EditIcon} alt="Edit" />,
+							label: "Edit",
+							onClick: () => {
+								/* TODO: edit logic */
+							},
+						},
+					]
+				: []),
+			{ divider: true },
+			{
+				icon: <img src={MuteIcon} alt="Mute" />,
+				label: "Mute",
+				onClick: () => {
+					/* TODO: mute logic */
+				},
+			},
+			{
+				icon: <img src={MarkAsReadIcon} alt="Mark as read" />,
+				label: "Mark as read",
+				onClick: () => {
+					/* TODO: mark as read logic */
+				},
+			},
+			{
+				icon: <img src={PreviewIcon} alt="Preview" />,
+				label: "Preview",
+				onClick: () => {
+					/* TODO: preview logic */
+				},
+			},
+			{ divider: true },
+			isOwner
+				? {
+						icon: <img src={TrashIcon} alt="Delete" />,
+						label: "Delete",
+						onClick: () => apiMethods.deleteChannel(chat.id),
+						danger: true,
+					}
+				: {
+						icon: <img src={TrashIcon} alt="Leave" />,
+						label: "Leave",
+						onClick: () => apiMethods.leaveChannel(chat.id),
+						danger: true,
+					},
+		];
+		contextMenu.open(e.clientX, e.clientY, items);
+	};
+
 	return (
-		<div
-			className={`${styles.chatItem} ${isActive ? styles.activeChat : ""} ${
-				isCollapsed ? styles.collapsed : ""
-			}`}
-			onClick={() => {
-				onSelectChat(chat);
-			}}
-		>
-			<ChatAvatar chat={chat} />
-			{!isCollapsed && (
-				<div className={styles.chatInfo}>
-					<div className={styles.chatNameWrapper}>
-						{getIcon()}
-						<span className={styles.chatName}>
-							{renderEmojisToJSX(chat.name)}
-						</span>
+		<>
+			<div
+				className={`${styles.chatItem} ${isActive ? styles.activeChat : ""} ${
+					isCollapsed ? styles.collapsed : ""
+				}`}
+				onClick={() => {
+					onSelectChat(chat);
+				}}
+				onContextMenu={handleContextMenu}
+			>
+				<ChatAvatar chat={chat} />
+				{!isCollapsed && (
+					<div className={styles.chatInfo}>
+						<div className={styles.chatNameWrapper}>
+							{getIcon()}
+							<span className={styles.chatName}>
+								{renderEmojisToJSX(chat.name)}
+							</span>
+						</div>
+						<div className={styles.chatMessage}>{lastMessageContent}</div>
 					</div>
-					<div className={styles.chatMessage}>{lastMessageContent}</div>
-				</div>
+				)}
+			</div>
+			{contextMenu.isOpen && (
+				<ContextMenu
+					x={contextMenu.x}
+					y={contextMenu.y}
+					items={contextMenu.items}
+					onClose={contextMenu.close}
+				/>
 			)}
-		</div>
+		</>
 	);
 };
 
