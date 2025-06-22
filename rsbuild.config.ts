0cbebaf3a700a4a1b30e57db3245bb1d1f6d587a
snakoleg <ochsherbina@gmail.com>
2025-06-22 18:18:54 +0300

    Update alias path in rsbuild and tsconfig

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,50 +6,51 @@ import { pluginTypedCSSModules } from "@rsbuild/plugin-typed-css-modules";
 const isDevelopment = process.env.NODE_ENV === "development";
 
 export default defineConfig({
-    plugins: [pluginPreact(), pluginTypedCSSModules(), pluginSass()],
-    html: {
-        template: "./index.html",
-    },
-    source: {
-        entry: {
-            index: "./src/index.tsx",
-        },
-        define: {
-            "process.env.CDN_BASE_URL": JSON.stringify(
-                process.env.CDN_BASE_URL || "https://media.foxochat.app/attachments/",
-            ),
-            "process.env.API_URL": JSON.stringify(
-                process.env.API_URL || "https://api.foxochat.app/",
-            ),
-            "import.meta.env.MODE": JSON.stringify(
-                process.env.NODE_ENV || "production",
-            ),
-            config: JSON.stringify({
-                cdnBaseUrl:
-                    process.env.CDN_BASE_URL || "https://media.foxochat.app/attachments/",
-                apiUrl: process.env.API_URL || "https://api.foxochat.app/",
-            }),
-        },
-        preEntry: isDevelopment ? ["preact/debug"] : [],
-    },
-    output: {
-        polyfill: "usage",
-        cssModules: {
-            namedExport: true,
-        },
-        copy: [{ from: "node_modules/workbox-*/**/*.js", to: "workbox/" }],
-    },
-    resolve: {
-        alias: {
-            "@": "./src/",
-            "@components": "./src/components",
-            "@icons": "./src/assets/svg",
-            "@hooks": "./src/hooks/",
-            "@services": "./src/services/",
-            "@utils": "./src/utils/",
-            "@store": "./src/store/",
-            "@lib": "./src/lib/",
-            "@interfaces": "./src/interfaces/",
-        },
-    },
+	plugins: [pluginPreact(), pluginTypedCSSModules(), pluginSass()],
+	html: {
+		template: "./index.html",
+	},
+	source: {
+		entry: {
+			index: "./src/index.tsx",
+		},
+		define: {
+			"process.env.CDN_BASE_URL": JSON.stringify(
+				process.env.CDN_BASE_URL || "https://media.foxochat.app/attachments/",
+			),
+			"process.env.API_URL": JSON.stringify(
+				process.env.API_URL || "https://api.foxochat.app/",
+			),
+			"import.meta.env.MODE": JSON.stringify(
+				process.env.NODE_ENV || "production",
+			),
+			config: JSON.stringify({
+				cdnBaseUrl:
+					process.env.CDN_BASE_URL || "https://media.foxochat.app/attachments/",
+				apiUrl: process.env.API_URL || "https://api.foxochat.app/",
+			}),
+		},
+		preEntry: isDevelopment ? ["preact/debug"] : [],
+	},
+	output: {
+		polyfill: "usage",
+		cssModules: {
+			namedExport: true,
+		},
+		copy: [{ from: "node_modules/workbox-*/**/*.js", to: "workbox/" }],
+	},
+	resolve: {
+		alias: {
+			"@": "./src/",
+			"@components": "./src/components",
+			"@icons": "./src/assets/svg",
+			"@assets": "./src/assets/",
+			"@hooks": "./src/hooks/",
+			"@services": "./src/services/",
+			"@utils": "./src/utils/",
+			"@store": "./src/store/",
+			"@lib": "./src/lib/",
+			"@interfaces": "./src/interfaces/",
+		},
+	},
 });
