a86dc7f68a7b727b00bacfe4c56847d80fccbd2c
snakoleg <ochsherbina@gmail.com>
2025-06-22 17:22:10 +0300

    Add option to center page content

--- src/components/Base/Page/Page.tsx
+++ src/components/Base/Page/Page.tsx
@@ -1,13 +1,15 @@
 import { ComponentChildren } from "preact";
 import * as styles from "./Page.module.scss";
+import { classNames } from "@utils/functions";
 
 interface PageProps {
 	children: ComponentChildren;
+    center?: boolean;
 }
 
-export const Page = ({ children }: PageProps) => {
+export const Page = ({ children, center }: PageProps) => {
 	return (
-		<div className={styles.page}>
+		<div className={classNames(styles.page, center && styles.centered)}>
 			{children}
 		</div>
 	);
