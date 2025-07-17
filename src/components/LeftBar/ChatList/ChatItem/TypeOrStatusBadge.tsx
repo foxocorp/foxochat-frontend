be88b44a9be2ebd5c2966c47d05d386601e50069
snakoleg <ochsherbina@gmail.com>
2025-07-18 00:02:29 +0300

    Add TypeOrStatusBadge component for chat items

---
+++ src/components/LeftBar/ChatList/ChatItem/TypeOrStatusBadge.tsx
@@ -0,0 +1,26 @@
+import { memo } from "preact/compat";
+import { ChannelType } from "foxochat.js";
+import * as styles from "./ChatItem.module.scss";
+import ChannelIcon from "@/assets/icons/left-bar/chat-list/channel.svg";
+import GroupIcon from "@/assets/icons/left-bar/chat-list/group.svg";
+
+interface TypeOrStatusBadgeProps {
+  type: ChannelType;
+  isOnline?: boolean;
+}
+
+export const TypeOrStatusBadge = memo(({ type, isOnline }: TypeOrStatusBadgeProps) => {
+  if (type === ChannelType.DM) {
+    if (!isOnline) return null;
+    return <div className={styles.typeBadge + " " + styles.onlineDot} />;
+  }
+  return (
+    <div className={styles.typeBadge}>
+      {type === ChannelType.Group ? (
+        <img src={GroupIcon} alt="Group" />
+      ) : type === ChannelType.Channel ? (
+        <img src={ChannelIcon} alt="Channel" />
+      ) : null}
+    </div>
+  );
+}); 
